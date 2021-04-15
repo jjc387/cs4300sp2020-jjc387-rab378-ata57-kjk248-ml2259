c818bcaf496ac5d9db0ae8b9b487dd7953401526
Rhea Bansal <rheaabansal@gmail.com>
2021-04-15 08:27:07 -0400

    function descriptions in search_controller.py

--- app/irsystem/controllers/search_controller.py
+++ app/irsystem/controllers/search_controller.py
@@ -18,4 +18,22 @@ def search():
 	return render_template('search.html', name=project_name, netid=net_id, output_message=output_message, data=data)
 
 
+def get_cos_sim(input, review):
+	"""
+	input: string- the users input
+	review: string - user reivew from dataset
+	"""
+
+def cos_sim_reviews(input_terms, reviews):
+	"""
+	input_terms: string inputted query
+	reviews: {} dictionary of the user responses in format wine_name: user reivew
+
+	1: get cos_sim of each one (into a arr)
+	2: arg sort the arr to get the index of the top ____
+	3: go through each row to contruct a {location: [ids]}
+	4: once there are 4 unique regions & at least 5 entries in each list of tuples
+
+	returns a dictionary of locations in format {location : [(score, row_number)]} 
+	"""
 

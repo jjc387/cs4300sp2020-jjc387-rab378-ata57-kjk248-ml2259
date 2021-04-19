d3faac07b6a30d76e2b6cc266cc3f70d6d3e512f
Amani Ahmed <amaniahmed299@gmail.com>
2021-04-19 11:17:32 -0400

    amani updated location and cosine sim methods

--- app/irsystem/controllers/search_controller.py
+++ app/irsystem/controllers/search_controller.py
@@ -2,6 +2,7 @@ from . import *
 from app.irsystem.models.helpers import *
 from app.irsystem.models.helpers import NumpyEncoder as NumpyEncoder
 from app.irsystem.controllers.wine_data import wine_dict
+from sklearn.metrics.pairwise import cosine_similarity
 
 project_name = "Wine and Beer Food Pairings"
 net_id = "Jessica Chen: jjc387, Rhea Bansal: rab378, Amani Ahmed: ata57, \
@@ -28,20 +29,47 @@ def create_OR_list(wine_dict, query):
 	"""
 
 
-def get_cos_sim(input, reviews, relevant_doc_index):
+def get_cos_sim(query, reviews, relevant_doc_index):
 	"""
 	input: string- the users input
 	reviews: user reviews (wine_dict)
 	relevant_doc_index: list of relevant docs
 	returns: {index: score}
 	"""
+	scores = {}
+	for doc in relevant_doc_index:
+		curr_review = reviews[doc]
+		cos_sim = cosine_similarity(query, curr_review)
+		scores[doc] = cos_sim
+	return scores
 
 
-def location_frequency(top_locations_dict):
+def location_frequency(scores_dict):
 	"""
-	get frequencies of the top 100 locations
+	get frequencies of the top 5 locations
 	return {location : (frequency, [index])}
 	"""
+	locs = {}
+	scores_list = [(x, scores_dict[x]) for x in scores_dict]
+	scores_list = sorted(scores_list, key = lambda x: x[1])
+	
+	i = 0
+	y = 500
+	while i < y and i < len(scores_list):
+		doc, score = scores_list[i]
+		prov = wine_dict[doc]['province']
+		if prov not in locs:
+			locs[prov]['id'] = [doc]
+			locs[prov]['frequency'] = 1
+		else:
+			locs[prov]['id'].append(doc)
+			locs[prov]['frequency'] += 1
+
+		i += 1
+		if i == y:
+			if len(locs) < 5:
+				y += 50		
+	return locs
 
 def cos_sim_reviews(input_terms, wine_dict):
 	"""

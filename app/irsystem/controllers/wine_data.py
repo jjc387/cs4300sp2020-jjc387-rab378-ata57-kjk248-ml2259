970b8730990e14d4495da253ae529f94b00f33ee
Mindy Lee <ml2259@cornell.edu>
2021-04-19 18:41:40 -0700

    dropped columns, cut rows

--- app/irsystem/controllers/wine_data.py
+++ app/irsystem/controllers/wine_data.py
@@ -3,8 +3,8 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 
 
 url = "https://raw.githubusercontent.com/jjc387/cs4300sp2021-jjc387-rab378-ata57-kjk248-ml2259/mindy/app/irsystem/winereviews.csv"
-column_headers = ["country", "description", "designation", "points", "price", "province", "region_1", "region_2", "variety", "winery"]
-df = pd.read_csv(url, usecols=column_headers)
+column_headers = ["description", "province",  "variety"]
+df = pd.read_csv(url, usecols=column_headers, nrows=75000)
 wine_dict = df.to_dict(orient='index')
 
 # referenced assignment 5 to help with the tf-idf vecotorizer!
